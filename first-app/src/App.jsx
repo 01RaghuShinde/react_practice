e8512cf8494ef505549b23b687ecd6784d7e1493
01RaghuShinde <raghuveershinde1249@gmail.com>
2025-02-05 21:49:40 +0530

    updated all projects

---
+++ first-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import { createBrowserRouter } from "react-router-dom";
+
+
+function App() {
+  const router =  createBrowserRouter([
+    {},
+    {},
+
+  ])
+  
+ 
+  return (
+    <>
+    <Nav/>
+      
+
+      
+
+      </>
+  )
+}
+
+export default App;
